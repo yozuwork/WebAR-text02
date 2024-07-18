5814bdb01e12551f856464b383f428d752ee61b7
yozu <yozuwork@gmail.com>
2024-07-18 10:54:58 +0800

    feat: Update fixed video component and add click functionality

--- assets/events.js
+++ assets/events.js
@@ -1,13 +1,30 @@
-AFRAME.registerComponent('image-tracker-1', {
+alert("hi");
+AFRAME.registerComponent('fixed-video-1', {
     init: function () {
-        console.log("image-tracker-1 init");
+        this.fixed_video_container = document.querySelector("#fixed-video-container-1");
+        this.fixed_video = document.querySelector("#fixed-video-1");
+        this.close_btn = document.querySelector("#fixed-video-container-1 .close-btn");
+        this.el.addEventListener("click", this.onClick.bind(this));
+        this.close_btn.addEventListener("click", e => {
+            this.toggle = false;
+            this.fixed_video_container.classList.remove("active");
+            this.fixed_video.pause();
+        });
     },
-    tick: function() {
-        if (this.el.object3D.visible == true) {
-            alert("掃描點1")
+    onClick: function() {
+        console.log("Clicked 1");
+        if (!this.toggle) {
+            this.toggle = true;
+            this.fixed_video_container.classList.add("active");
+            this.fixed_video.play();
+        } else {
+            this.toggle = false;
+            this.fixed_video_container.classList.remove("active");
+            this.fixed_video.pause();
         }
     }
 });
+
 // AFRAME.registerComponent('image-tracker-2', {
 //     init: function () {
 //         console.log("image-tracker-2 init");
