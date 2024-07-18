c2b1ad02ced04275d3b6c2e2fac57466d11e8579
yozu <yozuwork@gmail.com>
2024-07-18 10:00:16 +0800

    chore: Add image trackers for scanning points

---
+++ assets/events.js
@@ -0,0 +1,40 @@
+AFRAME.registerComponent('image-tracker-1', {
+    init: function () {
+        console.log("image-tracker-1 init");
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            alert("掃描點1")
+        }
+    }
+});
+AFRAME.registerComponent('image-tracker-2', {
+    init: function () {
+        console.log("image-tracker-2 init");
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            alert("掃描點2")
+        }
+    }
+});
+AFRAME.registerComponent('image-tracker-3', {
+    init: function () {
+        console.log("image-tracker-3 init");
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            alert("掃描點3")
+        }
+    }
+});
+AFRAME.registerComponent('image-tracker-4', {
+    init: function () {
+        console.log("image-tracker-4 init");
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            alert("掃描點4")
+        }
+    }
+});
