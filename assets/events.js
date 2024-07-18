8aacafb2b54d65c32e0d98ca3f94f1a9003be790
yozu <yozuwork@gmail.com>
2024-07-18 11:01:56 +0800

    feat: Refactor image tracker component and improve tracking

--- assets/events.js
+++ assets/events.js
@@ -1,57 +1,12 @@
-alert("hi");
-AFRAME.registerComponent('fixed-video-1', {
+
+
+AFRAME.registerComponent('image-tracker-1', {
     init: function () {
-        this.fixed_video_container = document.querySelector("#fixed-video-container-1");
-        this.fixed_video = document.querySelector("#fixed-video-1");
-        this.close_btn = document.querySelector("#fixed-video-container-1 .close-btn");
-        this.el.addEventListener("click", this.onClick.bind(this));
-        this.close_btn.addEventListener("click", e => {
-            this.toggle = false;
-            this.fixed_video_container.classList.remove("active");
-            this.fixed_video.pause();
-        });
+        alert("image-tracker-1 init");
     },
-    onClick: function() {
-        console.log("Clicked 1");
-        if (!this.toggle) {
-            this.toggle = true;
-            this.fixed_video_container.classList.add("active");
-            this.fixed_video.play();
-        } else {
-            this.toggle = false;
-            this.fixed_video_container.classList.remove("active");
-            this.fixed_video.pause();
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            alert("掃描點2")
         }
     }
 });
-
-// AFRAME.registerComponent('image-tracker-2', {
-//     init: function () {
-//         console.log("image-tracker-2 init");
-//     },
-//     tick: function() {
-//         if (this.el.object3D.visible == true) {
-//             alert("掃描點2")
-//         }
-//     }
-// });
-// AFRAME.registerComponent('image-tracker-3', {
-//     init: function () {
-//         console.log("image-tracker-3 init");
-//     },
-//     tick: function() {
-//         if (this.el.object3D.visible == true) {
-//             alert("掃描點3")
-//         }
-//     }
-// });
-// AFRAME.registerComponent('image-tracker-4', {
-//     init: function () {
-//         console.log("image-tracker-4 init");
-//     },
-//     tick: function() {
-//         if (this.el.object3D.visible == true) {
-//             alert("掃描點4")
-//         }
-//     }
-// });
