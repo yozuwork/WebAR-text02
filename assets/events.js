5e9d0fe96c54d600f43b2d993b822fb34f16111c
yozu <yozuwork@gmail.com>
2024-07-18 10:31:50 +0800

    chore: Remove unused image trackers for scanning points

--- assets/events.js
+++ assets/events.js
@@ -8,33 +8,33 @@ AFRAME.registerComponent('image-tracker-1', {
         }
     }
 });
-AFRAME.registerComponent('image-tracker-2', {
-    init: function () {
-        console.log("image-tracker-2 init");
-    },
-    tick: function() {
-        if (this.el.object3D.visible == true) {
-            alert("掃描點2")
-        }
-    }
-});
-AFRAME.registerComponent('image-tracker-3', {
-    init: function () {
-        console.log("image-tracker-3 init");
-    },
-    tick: function() {
-        if (this.el.object3D.visible == true) {
-            alert("掃描點3")
-        }
-    }
-});
-AFRAME.registerComponent('image-tracker-4', {
-    init: function () {
-        console.log("image-tracker-4 init");
-    },
-    tick: function() {
-        if (this.el.object3D.visible == true) {
-            alert("掃描點4")
-        }
-    }
-});
+// AFRAME.registerComponent('image-tracker-2', {
+//     init: function () {
+//         console.log("image-tracker-2 init");
+//     },
+//     tick: function() {
+//         if (this.el.object3D.visible == true) {
+//             alert("掃描點2")
+//         }
+//     }
+// });
+// AFRAME.registerComponent('image-tracker-3', {
+//     init: function () {
+//         console.log("image-tracker-3 init");
+//     },
+//     tick: function() {
+//         if (this.el.object3D.visible == true) {
+//             alert("掃描點3")
+//         }
+//     }
+// });
+// AFRAME.registerComponent('image-tracker-4', {
+//     init: function () {
+//         console.log("image-tracker-4 init");
+//     },
+//     tick: function() {
+//         if (this.el.object3D.visible == true) {
+//             alert("掃描點4")
+//         }
+//     }
+// });
